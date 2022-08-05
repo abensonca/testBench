c5b2f46415158d9ec97dabfd252bf78962aff74d
github-action-benchmark <github@users.noreply.github.com>
2022-08-05 18:35:50 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 3c04100578628d430fed6cb0721ddd069e944475

---
+++ dev/bench/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1659724550183,
+  "repoUrl": "https://github.com/abensonca/testBench",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Andrew Benson",
+            "username": "abensonca",
+            "email": "abenson@obs.carnegiescience.edu"
+          },
+          "committer": {
+            "name": "Andrew Benson",
+            "username": "abensonca",
+            "email": "abenson@obs.carnegiescience.edu"
+          },
+          "id": "3c04100578628d430fed6cb0721ddd069e944475",
+          "message": "fix: Set upstream",
+          "timestamp": "2022-08-05T18:35:23Z",
+          "url": "https://github.com/abensonca/testBench/commit/3c04100578628d430fed6cb0721ddd069e944475"
+        },
+        "date": 1659724549315,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "CPU",
+            "value": 50,
+            "unit": "%"
+          }
+        ]
+      }
+    ]
+  }
+}
